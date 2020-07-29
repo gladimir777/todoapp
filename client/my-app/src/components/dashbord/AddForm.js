5fbdb835ea20f9097929b08058883990b8eef541
gladimir777 <gladimir777@github.com>
2020-07-29 09:35:45 -0400

    Handle input change

--- client/my-app/src/components/dashbord/AddForm.js
+++ client/my-app/src/components/dashbord/AddForm.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddForm = () => {
+  const [task, setTask] = useState({ name: '', description: '' });
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <form>
         <div class="form-group">
           <label for="exampleFormControlInput1">Name</label>
           <input
-            type="email"
+            type="text"
             class="form-control"
             id="exampleFormControlInput1"
+            name="name"
+            value={task.name}
+            onChange={(e) => handleChange(e)}
             placeholder="Todo name"
           />
         </div>
@@ -20,6 +29,9 @@ const AddForm = () => {
             class="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
+            name="description"
+            value={task.description}
+            onChange={(e) => handleChange(e)}
             placeholder="A description of what need to be done"
           ></textarea>
         </div>
